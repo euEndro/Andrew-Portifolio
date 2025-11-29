ccb8dc3fb8080d6d520a2481dd8fa99792f9e8c1
euEndro <andrew.doni.assis@gmail.com>
2025-11-29 14:38:20 -0300

    trying some bs to make the site work

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Andrew-Portfolio",
+  base: "/Andrew-Portfolio/",
   optimizeDeps: {
     exclude: [],
   },
